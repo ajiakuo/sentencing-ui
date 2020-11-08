5bda12107b59a7cb4d5926a5738d43a410adf0c8
Poren Chiang <poren@poren.tw>
2020-11-08 20:09:00 +0800

    Wire up application layout in grid

--- src/App.js
+++ src/App.js
@@ -1,59 +1,76 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
-import Drawer from '@material-ui/core/Drawer';
-import Fab from '@material-ui/core/Fab';
-import Grid from '@material-ui/core/Grid';
-import makeStyles from '@material-ui/core/styles';
-import { Toolbar, IconButton, Typography } from '@material-ui/core';
-import { List, ListItem, ListItemIcon, ListItemText, Divider } from '@material-ui/core';
-import { Menu as MenuIcon, Feedback as FeedbackIcon, Help as HelpIcon, List as ListIcon, Spellcheck as FactCheckIcon } from '@material-ui/icons';
-import SentencingForm from 'parts/SentencingForm';
+import {
+  AppBar, Divider, Fab, Grid, IconButton, Paper, Toolbar, Typography
+  } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  AccountCircle as AccountIcon,
+  Feedback as FeedbackIcon,
+  Help as HelpIcon,
+  Spellcheck as FactCheckIcon,
+  } from '@material-ui/icons';
+import SentencingForm from './parts/SentencingForm';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: '100vh',
-  }
+  },
+  title: {
+    flexGrow: 1,
+  },
+  main: {
+    overflowX: 'hidden',
+  },
+  formPanel: {
+  },
+  form: {
+    padding: theme.spacing(2),
+  },
+  fab: {
+    margin: theme.spacing(2),
+  },
+  fabIcon: {
+    marginRight: theme.spacing(1),
+  },
+  contentPanel: {
+    padding: theme.spacing(2),
+  },
 }));
 
 export default function App() {
   const classes = useStyles();
 
   return (
-    <div>
-      <AppBar position="fixed">
+    <div className={classes.root}>
+      <AppBar position="sticky">
         <Toolbar>
-          <IconButton aria-label="開啟功能表" edge="start" color="inherit">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>量刑系統</Typography>
+          <Typography variant="h6" noWrap className={classes.title}>
+            司法院量刑系統
+          </Typography>
           <IconButton aria-label="問題回報" color="inherit">
             <FeedbackIcon />
           </IconButton>
           <IconButton aria-label="使用說明" color="inherit">
             <HelpIcon />
           </IconButton>
+          <Divider />
+          <IconButton aria-label="開啟功能表" color="inherit">
+            <AccountIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
-      <Drawer variant="permanent" anchor="right">
-        <List>
-          {['Lorem', 'Ipsum', 'Dolor', 'Amet'].map((text, _index) => (
-            <ListItem button key={text}>
-              <ListItemIcon><ListIcon /></ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-          <Divider />
-        </List>
-      </Drawer>
-      <Grid container component="main" className={classes.root}>
-        <Grid item xs={12} md={6} component={SentencingForm} />
-        <Grid item xs={12} md={6}>
-          <Fab color="secondary" variant="extended">
-            <FactCheckIcon />
+      <Grid container component="main" className={classes.main}>
+        <Grid item xs={12} md={6} className={classes.formPanel}>
+          <Paper elevation={3} className={classes.form}>
+            <SentencingForm />
+          </Paper>
+          <Fab color="secondary" variant="extended" className={classes.fab}>
+            <FactCheckIcon className={classes.fabIcon} />
             計算
           </Fab>
+        </Grid>
+        <Grid item xs={12} md={6} className={classes.contentPanel}>
+          Content
         </Grid>
       </Grid>
     </div>
--- src/parts/SentencingForm.js
+++ src/parts/SentencingForm.js
@@ -1,5 +1,5 @@
 import { React, Component } from 'react';
-import Paper from '@material-ui/core/Paper';
+import { Paper, Typography } from '@material-ui/core';
 import '../scope';
 
 class SentencingForm extends Component {
@@ -9,8 +9,20 @@ class SentencingForm extends Component {
 
   render() {
     return (
-      <Paper elevation={2}>
-      </Paper>
+      <div>
+        <Typography variant="h5">
+          哈囉，世界。
+        </Typography>
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Typography>
+      </div>
     );
   }
 }
