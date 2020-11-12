2e10d4c03919699a0584c660908ade3654b2adf2
Poren Chiang <poren@poren.tw>
2020-11-13 02:02:29 +0800

    Use snapshot functionality

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import CSSBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
 import theme from './theme';
 
-ReactDOM.render(
+render(
   <ThemeProvider theme={theme}>
     <CSSBaseline />
     <App />
