107a04c3ca83d1ba3c916eb34a4e52e2f9b77e42
Poren Chiang <poren@poren.tw>
2020-11-13 18:07:23 +0800

    Fine tune accordion

--- src/parts/SentencingForm.js
+++ src/parts/SentencingForm.js
@@ -1,11 +1,27 @@
 import { React } from 'react';
 import { Accordion, AccordionSummary, AccordionDetails, FormControl, FormHelperText, Grid, InputLabel, MenuItem, Select, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { factors, factorGroups } from '../scope';
 import FactorCheckBox from '../controls/FactorCheckBox';
 import FactorSlider from '../controls/FactorSlider';
 
-const renderGroup = (group) => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: 0,
+  },
+  summary: {
+    paddingLeft: 0,
+  },
+  details: {
+    padding: 0,
+    flexDirection: 'column',
+  }
+}), { name: 'GroupAccordion' });
+
+const GroupAccordion = (props) => {
+  const classes = useStyles();
+  const group = props.group;
   const renderItem = (
     group.type === "binary" ?
     (factor) => <FactorCheckBox name={factor.name} label={factor.text} vibe={group.vibe} /> :
@@ -13,11 +29,11 @@ const renderGroup = (group) => {
   );
 
   return (
-    <Accordion elevation={0} defaultExpanded={group.type !== "binary"}>
-      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+    <Accordion elevation={0} defaultExpanded={group.type !== "binary"} className={classes.root}>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />} className={classes.summary}>
         <Typography variant="h6">{group.title}</Typography>
       </AccordionSummary>
-      <AccordionDetails style={ {flexDirection: 'column'} }>
+      <AccordionDetails className={classes.details}>
       { group.factors.map((name) => {
         let factor = factors.find(i => i.name === name);
         return (!factor.valid_before && (
@@ -43,10 +59,12 @@ export default function SentencingForm() {
           <FormHelperText>§367 吞食玻璃不傷身體罪</FormHelperText>
         </FormControl>
       </div>
-      <Grid container spacing={2}>
+      <Grid container spacing={4}>
         { ["tristate", "binary"].map((type) => (
           <Grid item xs={12} sm={6} md={12} lg={6}>
-            { factorGroups.filter(group => group.type === type).map(renderGroup) }
+            { factorGroups.filter(group => group.type === type).map((group) => (
+              <GroupAccordion group={group} />
+            )) }
           </Grid>
         )) }
       </Grid>
