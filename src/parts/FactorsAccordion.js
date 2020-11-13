e5bc5aef0dd012593f49250cb82d388a6c86bdcb
Poren Chiang <poren@poren.tw>
2020-11-13 18:57:16 +0800

    Separate accordion into its own component

---
+++ src/parts/FactorsAccordion.js
@@ -0,0 +1,77 @@
+import { React } from 'react';
+import { Accordion, AccordionSummary, AccordionDetails, Typography } from '@material-ui/core';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { factors } from '../scope';
+import FactorCheckBox from '../controls/FactorCheckBox';
+import FactorSlider from '../controls/FactorSlider';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: 0,
+  },
+  summary: {
+    paddingLeft: 0,
+  },
+  details: {
+    padding: 0,
+    flexDirection: 'column',
+  }
+}), { name: 'FactorsAccordion' });
+
+const CondenseAccordion = withStyles(() => ({
+  root: {
+    '&$expanded': {
+      margin: 0,
+      '&:before': {
+        opacity: 1,
+      },
+    },
+  },
+  expanded: {},
+}), { name: 'CondenseAccordion' })(Accordion);
+
+const CondenseAccordionSummary = withStyles(() => ({
+  root: {
+    '&$expanded': {
+      minHeight: 'auto',
+    }
+  },
+  content: {
+    margin: '12px 0',
+    '&$expanded': {
+      margin: '12px 0',
+    }
+  },
+  expanded: {},
+}), { name: 'CondenseAccordionSummary' })(AccordionSummary);
+
+export default function FactorsAccordion(props) {
+  const classes = useStyles();
+
+  // Helpers
+  const group = props.group;
+  const renderItem = (
+    group.type === "binary" ?
+    (factor) => <FactorCheckBox name={factor.name} label={factor.text} vibe={group.vibe} /> :
+    (factor) => <FactorSlider name={factor.name} label={factor.text} />
+  );
+
+  return (
+    <CondenseAccordion elevation={0} defaultExpanded={group.type !== "binary"} className={classes.root}>
+      <CondenseAccordionSummary expandIcon={<ExpandMoreIcon />} className={classes.summary}>
+        <Typography variant="h6">{group.title}</Typography>
+      </CondenseAccordionSummary>
+      <AccordionDetails className={classes.details}>
+      { group.factors.map((name) => {
+        let factor = factors.find(i => i.name === name);
+        return (!factor.valid_before && (
+          <div>
+            {renderItem(factor)}
+          </div>
+        ));
+      })}
+      </AccordionDetails>
+    </CondenseAccordion>
+  );
+};
--- src/parts/SentencingForm.js
+++ src/parts/SentencingForm.js
@@ -1,51 +1,8 @@
 import { React } from 'react';
-import { Accordion, AccordionSummary, AccordionDetails, FormControl, FormHelperText, Grid, InputLabel, MenuItem, Select, Typography } from '@material-ui/core';
+import { FormControl, FormHelperText, Grid, InputLabel, MenuItem, Select, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { factors, factorGroups } from '../scope';
-import FactorCheckBox from '../controls/FactorCheckBox';
-import FactorSlider from '../controls/FactorSlider';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    margin: 0,
-  },
-  summary: {
-    paddingLeft: 0,
-  },
-  details: {
-    padding: 0,
-    flexDirection: 'column',
-  }
-}), { name: 'GroupAccordion' });
-
-const GroupAccordion = (props) => {
-  const classes = useStyles();
-  const group = props.group;
-  const renderItem = (
-    group.type === "binary" ?
-    (factor) => <FactorCheckBox name={factor.name} label={factor.text} vibe={group.vibe} /> :
-    (factor) => <FactorSlider name={factor.name} label={factor.text} />
-  );
-
-  return (
-    <Accordion elevation={0} defaultExpanded={group.type !== "binary"} className={classes.root}>
-      <AccordionSummary expandIcon={<ExpandMoreIcon />} className={classes.summary}>
-        <Typography variant="h6">{group.title}</Typography>
-      </AccordionSummary>
-      <AccordionDetails className={classes.details}>
-      { group.factors.map((name) => {
-        let factor = factors.find(i => i.name === name);
-        return (!factor.valid_before && (
-          <div>
-            {renderItem(factor)}
-          </div>
-        ));
-      })}
-      </AccordionDetails>
-    </Accordion>
-  );
-}
+import { factorGroups } from '../scope';
+import FactorsAccordion from './FactorsAccordion';
 
 export default function SentencingForm() {
   return (
@@ -63,7 +20,7 @@ export default function SentencingForm() {
         { ["tristate", "binary"].map((type) => (
           <Grid item xs={12} sm={6} md={12} lg={6}>
             { factorGroups.filter(group => group.type === type).map((group) => (
-              <GroupAccordion group={group} />
+              <FactorsAccordion group={group} />
             )) }
           </Grid>
         )) }
