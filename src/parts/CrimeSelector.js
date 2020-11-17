214eac0bbf46c2b0b547b02625f8e496b848353f
Poren Chiang <poren@poren.tw>
2020-11-17 16:11:32 +0800

    Implement basic shape of CrimeSelector

--- src/parts/CrimeSelector.js
+++ src/parts/CrimeSelector.js
@@ -1,6 +1,6 @@
 import { React, useState } from 'react';
 //import SwipeableViews from 'react-swipeable-views';
-import { Tabs, Tab, Typography, Box, FormControl, FormLabel, FormControlLabel, Radio, RadioGroup } from '@material-ui/core';
+import { Tabs, Tab, ButtonGroup, Button, FormControl, Typography, FormControlLabel, Radio } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -9,6 +9,19 @@ const useStyles = makeStyles((theme) => ({
   },
   tab: {
     minWidth: 64,
+    flexGrow: 1,
+  },
+  tabPage: {
+    margin: theme.spacing(1),
+  },
+  stages: {
+    display: 'flex',
+    marginTop: theme.spacing(1),
+  },
+  stageLabel: {
+    marginBottom: theme.spacing(1),
+  },
+  stage: {
     flexGrow: 1,
   },
 }), { name: 'CrimeSelector' });
@@ -30,14 +43,19 @@ export default function CrimeSelector(props) {
         <Tab label="其他" className={classes.tab} />
       </Tabs>
       <div>
-        <div id="crime-panel-1">{ /* aria-labelled-by="crime-tab-1" --> */ }
+        <div id="crime-panel-1" className={classes.tabPage}>{ /* aria-labelled-by="crime-tab-1" --> */ }
           <FormControl component="fieldset">
-            <FormLabel component="legend">罪名</FormLabel>
-            <RadioGroup aria-label="罪名" onChange={handleRadioChange}>
-              { ["普通殺人", "殺害直系血親尊親屬", "義憤殺人", "母殺嬰兒", "加工自殺"].map((name, i) =>
-                <FormControlLabel key={i} value={i} control={<Radio />} label={name} />
-              ) }
-            </RadioGroup>
+            { ["普通殺人", "殺害直系血親尊親屬", "義憤殺人", "母殺嬰兒", "加工自殺"].map((name, i) =>
+              <FormControlLabel key={i} value={i} checked={kind == i} control={<Radio />} label={name} onClick={handleRadioChange} />
+            ) }
+          </FormControl>
+          <FormControl className={classes.stages}>
+            <Typography variant="subtitle2" gutterBottom>階段</Typography>
+            <ButtonGroup color="primary" aria-label="犯罪階段">
+              { kind == 0 && <Button value="preparatory" className={classes.stage}>預備</Button> }
+              <Button value="attempted" className={classes.stage}>未遂</Button>
+              <Button value="accomplished" className={classes.stage}>既遂</Button>
+            </ButtonGroup>
           </FormControl>
         </div>
       </div>
