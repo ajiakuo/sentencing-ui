fd562eee906906effc3c86abeeaeeaafaea3e1cd
Poren Chiang <poren@poren.tw>
2020-12-11 12:36:46 +0800

    Add help icon

--- src/parts/Page.js
+++ src/parts/Page.js
@@ -1,7 +1,8 @@
 import { React, useState } from 'react';
-import { AppBar, Avatar, CircularProgress, Grid, Paper, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Avatar, CircularProgress, Grid, IconButton, Paper, Toolbar, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import GavelIcon from '@material-ui/icons/Gavel';
+import HelpIcon from '@material-ui/icons/HelpOutline';
 import FormToolbar from '../controls/FormToolbar';
 import AppForm from './AppForm';
 import AppMenu from './AppMenu';
@@ -67,11 +68,15 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: '100%',
   },
   descriptor: {
+    color: theme.palette.text.secondary,
     marginTop: theme.spacing(1),
     marginBottom: theme.spacing(1),
     '& + .Mui-expanded': {
       marginTop: 0,
     },
+  },
+  helpIcon: {
+    fontSize: '1rem',
   },
   message: {
     color: theme.palette.text.secondary,
@@ -149,7 +154,7 @@ export default function App() {
             }
             { status === 'ready' &&
               <>
-                <Typography variant="overline" component="h5" className={classes.descriptor}>量刑預測</Typography>
+                <Typography variant="caption" component="h5" className={classes.descriptor}>量刑預測</Typography>
                 <Paper elevation={1} className={classes.prediction}>
                   <Typography variant="h4" component="div">{
                     data.estimation > data.error_margin ?
@@ -160,7 +165,12 @@ export default function App() {
                     <img src={data.plot_img} className={classes.plot} />
                   }
                 </Paper>
-                <Typography variant="overline" component="h5" className={classes.descriptor}>相關判決</Typography>
+                <Typography variant="caption" component="h5" className={classes.descriptor}>
+                  相關判決
+                  <IconButton aria-label="說明" size="small">
+                    <HelpIcon className={classes.helpIcon} />
+                  </IconButton>
+                </Typography>
                 { data.related_cases.map((i) =>
                   <CaseAccordion key={i.id} {...i} />
                 )}
